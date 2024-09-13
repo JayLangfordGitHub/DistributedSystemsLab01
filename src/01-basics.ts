9b4800100c0764e21e543902dc2f3d7116b89e7f
JayLangfordGitHub <jaylangfordprf@gmail.com>
2024-09-13 10:00:56 +0100

    Added type-safety to friend data.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -4,13 +4,24 @@ interface Friend {
     age: number
 }
 
-const friend1 = {
+interface Colleague {
+    name: string;
+    department: string;
+    contact: ContactInfo;
+  }
+
+interface ContactInfo {
+    email: string;
+    extension: number;
+  }
+
+const friend1: Friend = {
     name: "Paul Fleming",
     phone: "087-12345",
     age: 25,
   };
   
-  const friend2 = {
+  const friend2: Friend = {
     name: "Jane Costello",
     phone: "086--12345",
     age: 31,
@@ -20,7 +31,7 @@ const friend1 = {
   console.log(friends[1]);
   
   //   -------------------
-  const colleague1 = {
+  const colleague1: Colleague = {
     name: "Ralph Graham",
     department: "Engineering",
     contact: {
@@ -29,7 +40,7 @@ const friend1 = {
     },
   };
   
-  const colleague2 = {
+  const colleague2: Colleague = {
     name: "Patti Burke",
     department: "Finance",
     contact: {
@@ -38,7 +49,7 @@ const friend1 = {
     },
   };
   
-  const colleague3 = {
+  const colleague3: Colleague = {
     name: "Dean Sullivan",
     department: "HR",
     contact: {
